e5919cedb14d2116c3103fff735edf31ce5cbc8c
Oli Lane <oli@gearset.com>
2017-09-03 11:54:00 +0100

    Add some untested code to output the price of bitcoin in USD whenever a price is asked for

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 'use strict';
 
 const Alexa = require('alexa-sdk');
+const cryptoPrices = require('./crypto-prices');
 
 const APP_ID = undefined; // TODO replace
 
 function generateHint(){
     return 'Try asking for the price of Bitcoin.';
+}
+
+function getCurrencySymbol(currencyInput) {
+    return 'BTC';
+}
+
+function getPriceSummarySpeech(currencyFrom, currencyTo, price){
+    return 'The price of ' + currencyFrom + ' right now is ' + price + ' ' + currencyTo;
 }
 
 const handlers = {
@@ -16,7 +25,19 @@ const handlers = {
         this.emit(':tell', speechOutput, reprompt);
     },
     'GetPriceIntent': function () {
-        
+        const currencyFromSlot = this.event.request.intent.slots.CurrencyFrom;
+        let currencyFrom;
+        if (currencyFromSlot && currencyFromSlot.value) {
+            currencyFrom = currencyFrom.value.toLowerCase();
+        }
+
+        let currencyFromSymbol = getCurrencySymbol(currencyFrom);
+
+        let price = cryptoPrices.getPrice(currencyFromSymbol, 'USD');
+
+        let priceOutput = getPriceSummarySpeech(currencyFrom, 'USD');
+
+        this.emit(':tell', priceOutput);
     },
     'AMAZON.HelpIntent': function () {
         let speechOutput = 'You can ask things like, ' + generateHint();
